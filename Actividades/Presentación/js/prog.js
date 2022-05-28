948a812cfdbeb97f1a37615954e22864274fa001
Luis Guere <daimanger@live.com.mx>
2022-05-28 14:16:15 -0500

    Actividades 1

---
+++ Actividades/Presentación/js/prog.js
@@ -0,0 +1,197 @@
+// 1
+var i = 0;
+function move1() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+//2
+var i = 0;
+function move2() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//3
+var i = 0;
+function move3() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//4
+var i = 0;
+function move4() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//5
+
+var i = 0;
+function move5() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//6
+
+var i = 0;
+function move6() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//7
+
+var i = 0;
+function move7() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//8
+
+var i = 0;
+function move8() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//9
+
+var i = 0;
+function move9() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+//10
+
+var i = 0;
+function move10() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 80) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
